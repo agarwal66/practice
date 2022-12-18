744ebb92bf4396a60d9f3ba42bf900f8ff74652c
Prateek Agarwal <114166286+agarwal66@users.noreply.github.com>
2022-12-18 11:22:50 +0530

    Add files via upload

---
+++ index18.js
@@ -0,0 +1,48 @@
+var q = document.getElementById("Hey");
+q.innerHTML = "Hey";
+var q = document.getElementById("my");
+q.innerHTML = "my";
+var q = document.getElementById("n");
+q.innerHTML = "name";
+var q = document.getElementById("is");
+q.innerHTML = "is";
+var q = document.getElementById("and");
+q.innerHTML = "and";
+var q = document.getElementById("my2");
+q.innerHTML = "my";
+var q = document.getElementById("a");
+q.innerHTML = "age";
+var q = document.getElementById("is2");
+q.innerHTML = "is";
+var q = document.getElementById("I");
+q.innerHTML = "I";
+var q = document.getElementById("love");
+q.innerHTML = "love";
+ const n = prompt("enter your name");
+ const nameElement = document.getElementById("name")
+ nameElement.innerHTML = "<em>" + n + "<em>"
+ const age = prompt("enter your age");
+ const ageElement = document.getElementById("age")
+ nameElement.innerHTML = "<em>" + n + "<em>"
+
+ const sub = prompt("enter your sub");
+ const subElement = document.getElementById("sub")
+ nameElement.innerHTML = "<em>" + n + "<em>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const p1Element= document.getElementById("p1")
+//console.log(p1Element)
+//const paragraphTagElement = document.getElementById("paragraphTagElement")
+//console.log(paragraphTagElement)
